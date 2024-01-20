e473eee41bca90017bc64f9e1f35017dbad9c1af
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-01-20 14:59:29 +0530

    Customer model

---
+++ src/main/java/com/springframework/exceltodatabase/domain/Customer.java
@@ -0,0 +1,22 @@
+package com.springframework.exceltodatabase.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+    @Id
+    private Integer customerId;
+    private String firstName;
+    private String lastName;
+    private String country;
+    private Integer telephone;
+}
